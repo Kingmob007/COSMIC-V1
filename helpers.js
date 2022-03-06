a705919e4e02fbc9ed7449de13221d3a8e9abcbc
KHADHER56 <101047534+KHADHER56@users.noreply.github.com>
2022-03-07 01:04:57 +0530

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *AMRU-SER*:  ```" + msg + "```"
+    return "✅ *COSMIC V1*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *AMRU-SER*:  ```" + msg + "```"
+    return "🛑 *COSMIC V1*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *AMRU-SER*:  ```" + msg + "```"
+    return "⏺️ *COSMIC V1*:  ```" + msg + "```"
 }
 
 
