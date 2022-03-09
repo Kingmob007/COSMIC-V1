5c429c73e1f6fe08b9959676a6c9eaad5c57ef46
KHADHER <96767020+Khadher05@users.noreply.github.com>
2022-03-09 07:21:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ module.exports = {
     STICKERP: process.env.AUTO_STICKER === undefined ? true : convertToBool(process.env.AUTO_STICKER),
     DISSTICKER: process.env.DISABLE_STICKER === undefined ? false : process.env.DISABLE_STICKER,
     BOT: process.env.BOT_NAME === undefined ? '⊢‒‒‒ ⋈ ᴄᴏꜱᴍɪᴄ ᴠ1 ⋈ ‒‒‒⊣' : process.env.BOT_NAME,
-    KAZTRO_SER: process.env.ALIVE_BUTTON === undefined ? 'Sed/Happy' : process.env.ALIVE_BUTTON,   
+    KAZTRO_SER: process.env.ALIVE_BUTTON === undefined ? 'KHADHER/COSMICV1' : process.env.ALIVE_BUTTON,   
     NOLOG: process.env.NO_LOG === undefined ? 'true' : process.env.NO_LOG,
     THERI_KICK: process.env.THERI_KICK === undefined ? 'false' : process.env.THERI_KICK,
     SONGD: process.env.SONGD === undefined ? 'ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ꜱᴏɴɢ🎶' : process.env.SONGD,
